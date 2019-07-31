e51536282b6b5945c77add987a632e1b6c5be14f
pedro <pedrohlcastro@gmail.com>
2019-07-31 08:00:28 -0300

    feat(material): add imports and config

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
 import { connect } from 'react-redux';
+
+import Button from '@material-ui/core/Button';
 
 const Main = () => (
   <div>
-    <h1>Main</h1>
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
   </div>
 );
 
